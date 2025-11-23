69090b2677f21e0eb273c251bd2d2a92b88ce875
guilherme-silva <guilherme.cristaldo15@gmail.com>
2025-11-23 01:00:59 -0300

    done sixth exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let stringConcated = "";
+    if (num >= 0) {
+        if(num === 0) return '';
+
+        for (let i = 0; i < num; i++) {
+           stringConcated = stringConcated + string;
+        }
+
+        return stringConcated
+    } else {
+        return "ERROR"
+    }
 
 };
+
+console.log(repeatString('hey', 5))
 
 // Do not edit below this line
 module.exports = repeatString;
