de63d8f6f66266cc0d2fef7a10e192a25546677e
guilherme-silva <guilherme.cristaldo15@gmail.com>
2025-11-23 01:08:48 -0300

    done seventh exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = string.split("").reverse().join("");
 
+    return reversedString;
 };
+
+console.log(reverseString("hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
